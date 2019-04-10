16ab254fbe10de288bb005619a5b1ead69fed822
miracleyou <786671043@qq.com>
2019-04-10 15:22:40 +0800

    rm print

--- gen_spara/sep_train.py
+++ gen_spara/sep_train.py
@@ -129,5 +129,5 @@ for idx_1 in range(2):
                 torch.save(netG.state_dict(), os.path.join(opt.outf, 'netG0_direct_choice_imi'+str(idx_1)+'_'+str(idx_2)+'.pth'))
                 log("Better model saved! New training loss:%f"%(train_loss))
             log('Epoch [%d/%d], Train Loss: %.6f' % (epoch + 1, opt.niter, train_loss))
-        exit()
+        # exit()
            
